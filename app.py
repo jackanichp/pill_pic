2941af24817ac7cb0df53427420e431460654eb1
Paul Jackanich <jackanichp@gmail.com>
2023-05-18 19:53:02 -0400

    sidebar created for streamlit

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 import streamlit as st
 
-def set_background_color(color):
-    hex_color = '#%02x%02x%02x' % color
-    css = f"""
-        <style>
-        body {{
-            background-color: {hex_color};
-        }}
-        </style>
-    """
-    st.markdown(css, unsafe_allow_html=True)
+st.title("Pill Pℹ️c 💊")
+
+with st.sidebar:
+    st.markdown("# About")
+    st.markdown(
+        "With Pill Pℹ️c, you can snap a photo 📷 of any pill, and \n"
+        "our image classification model trained on over 130K 🖼️ \n"
+        "images will recognize the pill type, dosage and manufacturer! \n"
+        "In addition, we'll supply you with detailed information about \n"
+        "your medication, such as possible interactions, allergies."
+        )
+    st.markdown(
+        "Pill Pℹ️c also allows you to create a user profile \n"
+        "so you can keep track of our medication history! \n"
+    )
+    st.markdown("---")
+    st.markdown("A group project by Morgane, Ninaad and Paul")
 
 def upload_and_store_picture():
+
     st.title("Picture Upload")
+
     uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
     if uploaded_file is not None:
         # Save the uploaded file on the server
         with open(uploaded_file.name, "wb") as file:
             file.write(uploaded_file.read())
-        st.success("Image successfully uploaded and stored!")
 
-# Set the background color to light blue
-set_background_color((173, 216, 230))
+        st.success("Image successfully uploaded and stored!")
 
 # Usage
 upload_and_store_picture()
