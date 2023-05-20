8945741a208b652fd458b2b3c7e7fa9420e65e05
Morgane Salvignol <morgane.salvignol@hec.ca>
2023-05-20 17:17:14 -0400

    added api folder

--- api/fast.py
+++ api/fast.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import random
+import numpy as np
+import cv2
+import io
+
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import Response
+from io import BytesIO
+from PIL import Image
 
 app = FastAPI()
 
@@ -14,12 +21,22 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-@app.post('/predict')
-def predict(image: UploadFile = File(...)):
-    # Compute `wait_prediction` from `day_of_week` and `time`
+@app.post('/upload_image')
+async def predict(img: UploadFile = File(...)):
+
+    # Return random drug name
     drug_prediction = random.choice(["TRI-LEGEST Fe","NORTEL 7/7/7","amrix","TRIVORA TAB","AMARYL 4MG TABLETS"])
 
-    return {'Pill Name': drug_prediction}
+    # Encoding and responding with the image
+    contents = await img.read()
+    '''image_io = BytesIO(contents)
+    image_pil = Image.open(image_io)
+    cv2_img = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)'''
+
+    nparr = np.fromstring(contents, np.uint8)
+    cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
+
+    return {"pill_name": drug_prediction}
 
 @app.get("/")
 def root():
